7a928524905ddd244232a04ad7efd02188ada30c
Elise Guezel <elise.guezel@yahoo.fr>
2020-01-29 17:15:14 +0100

    Create install.sh

---
+++ install.sql
@@ -0,0 +1,36 @@
+
+DROP DATABASE IF EXISTS wildcircus;
+CREATE DATABASE IF NOT EXISTS wildcircus;
+USE wildcircus;
+
+DROP TABLE IF EXISTS user, reservation, representation;
+
+CREATE TABLE user (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(128) NOT NULL UNIQUE,
+  password VARCHAR(128) NOT NULL,
+  isAdmin BOOLEAN NOT NULL
+);
+
+CREATE TABLE representation (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  city VARCHAR(255) NOT NULL,
+  date DATE NOT NULL,
+  availablePlaces INT NOT NULL,
+  price FLOAT NOT NULL,
+  photo TEXT,
+  coordinates VARCHAR(255)
+);
+
+CREATE TABLE reservation (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id)
+    REFERENCES user(id)
+    ON DELETE CASCADE,
+  representation_id INT NOT NULL,
+  FOREIGN KEY (representation_id)
+    REFERENCES representation(id)
+    ON DELETE CASCADE,
+  places INT NOT NULL
+);
