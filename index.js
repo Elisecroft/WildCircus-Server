cc0b1423b85ec7a9010a06777d1c5f2a5691429c
Elise Guezel <elise.guezel@yahoo.fr>
2020-01-30 09:18:52 +0100

    Create representatiosn route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const users = require('./routes/users');
+const representations = require('./routes/representations');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/users', users);
+app.use('/representations', representations);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ routes/representations.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const connection = require('../conf');
+const router = express.Router();
+
+// post
+router.post('/', (req, res) => {
+  const { city, date, availablePlaces, price, photo, coordinates } = req.body; // representation infos sent
+  if (!city || !date || !availablePlaces || !price) {
+    res.status(400).json('missing fields');
+  } else {
+    data = { city, date, availablePlaces, price, photo, coordinates };
+    connection.query('INSERT INTO representation SET ?', data, (error, result) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Error when saving representation');
+      } else {
+        connection.query('SELECT * FROM representation WHERE id = ?', [result.insertId], (subErr, subResult) => {
+          if (subErr) {
+            console.log(subErr);
+          } else {
+            res.status(201).send(subResult);
+          }
+        });
+      }
+    })
+  }
+});
+
+// get
+router.get('/:representationId', (req, res) => {
+  const representationId = req.params.representationId;
+  connection.query('SELECT * FROM representation WHERE id = ?', [representationId], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+// put
+router.put('/:representationId', (req, res) => {
+  const representationId = req.params.representationId;
+  const data = req.body; // representation infos sent
+  connection.query('UPDATE representation SET ? WHERE id = ?', [data, representationId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error when update representation');
+    } else {
+      connection.query('SELECT * FROM representation WHERE id = ?', [representationId], (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).send(result);
+        }
+      });
+    }
+  });
+});
+
+// delete
+router.delete('/:representationId', (req, res) => {
+  const representationId = req.params.representationId;
+  connection.query('DELETE FROM representation WHERE id = ?', [representationId], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send('Representation delete');
+    }
+  });
+});
+
+module.exports = router;
